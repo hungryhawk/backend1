bf6d6ee60a7f29412e8c62012dba4b8b81397e96
Pavel Emelianov <emeliarulit@gmail.com>
2023-05-12 00:58:37 +0300

    Add 404 and 500 routes

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ app.use(morgan('dev'));
 app.use('/api', require('./routes/userRoute'));
 app.use('/api', require('./routes/blockRoute'));
 
+app.use((req, res) =>
+  res.status(404).send({
+    error:
+      'Oops! Endpoint not found, Please Check that you are entering the right thing!',
+  })
+);
+app.use((err, req, res, next) => {
+  res.status(500).send({
+    error:
+      'Invalid Request! Please Check that you are entering the right thing!',
+  });
+});
+
 app.listen(5000, () => {
   console.log('At port 5000');
 });
